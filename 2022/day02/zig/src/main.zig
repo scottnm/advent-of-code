af4e9bd614ee1e7237a2a3803d0a24a9c3bd072c
scott munro <scottnmunro@gmail.com>
2022-12-04 15:33:17 -0800

    doing some very simple printing and arg parsing. a little more interesting than hello world

--- 2022/day02/zig/src/main.zig
+++ 2022/day02/zig/src/main.zig
@@ -1,24 +1,43 @@
 const std = @import("std");
 
+pub fn print_usage_and_exit(prog_name: []const u8) noreturn {
+    std.debug.print("USAGE: {s} [input_file]\n", .{prog_name});
+    std.process.exit(0);
+}
+
 pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+    const allocator = std.heap.page_allocator;
+    const args = try std.process.argsAlloc(allocator);
+    defer std.process.argsFree(allocator, args);
 
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
+    if (args.len < 2) {
+        print_usage_and_exit(args[0]);
+    }
 
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
-
-    try bw.flush(); // don't forget to flush!
+    std.debug.print("Done\n", .{});
 }
 
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
-}
+// FIXME: old stub main
+// pub fn main() !void {
+//     // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
+//     std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+//
+//     // stdout is for the actual output of your application, for example if you
+//     // are implementing gzip, then only the compressed bytes should be sent to
+//     // stdout, not any debugging messages.
+//     const stdout_file = std.io.getStdOut().writer();
+//     var bw = std.io.bufferedWriter(stdout_file);
+//     const stdout = bw.writer();
+//
+//     try stdout.print("Run `zig build test` to run the tests.\n", .{});
+//
+//     try bw.flush(); // don't forget to flush!
+// }
+
+// FIXME: stale test from the stub file creation
+// test "simple test" {
+//     var list = std.ArrayList(i32).init(std.testing.allocator);
+//     defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
+//     try list.append(42);
+//     try std.testing.expectEqual(@as(i32, 42), list.pop());
+// }
