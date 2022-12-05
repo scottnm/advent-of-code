9f71e99bbb6559e8f9fbd32a688ed96f919cccfa
scott munro <scottnmunro@gmail.com>
2022-12-04 17:25:19 -0800

    update my clang build script to be runnable from a win git bash so I can compare the performance of the c version with the zig version

--- 2022/day02/c/day02_build.sh
+++ 2022/day02/c/day02_build.sh
@@ -2,7 +2,7 @@
 script_root=$(dirname $(realpath "${BASH_SOURCE:-$0}"))
 
 clear
-rm "$script_root/day02.out"
-clang "$script_root/day02.c" "$script_root/../../../helpers/helpers.c" -I "$script_root/../../../helpers/" -o "$script_root/day02.out" -Wno-format-extra-args
-"$script_root/day02.out" "$script_root/day02_simple.txt" 
-"$script_root/day02.out" "$script_root/day02_real.txt"
+rm "$script_root/day02.exe"
+clang "$script_root/day02.c" "$script_root/../../../helpers/helpers.c" -I "$script_root/../../../helpers/" -o "$script_root/day02.exe" -Wno-format-extra-args -Wno-deprecated-declarations -O2
+"$script_root/day02.exe" "$script_root/day02_simple.txt" 
+"$script_root/day02.exe" "$script_root/day02_real.txt"
