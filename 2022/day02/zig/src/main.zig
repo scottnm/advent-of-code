d8787630c2205c62b026eac7d5054b35cd05f843
scott munro <scottnmunro@gmail.com>
2022-12-04 17:01:05 -0800

    ayyyyyyyyyyy lmao. got the file reading/writing working

--- 2022/day02/zig/src/main.zig
+++ 2022/day02/zig/src/main.zig
@@ -3,6 +3,7 @@ const std = @import("std");
 const RpsChoice = enum { rock, paper, scissors };
 const RpsResult = enum { lose, draw, win };
 const RoundData = struct { opponent_choice: RpsChoice, player_choice: RpsChoice };
+const ReadInputError = error{InvalidLineFormat};
 
 pub fn main() !void {
     const allocator = std.heap.page_allocator;
@@ -35,8 +36,52 @@ pub fn printUsageAndExit(prog_name: []const u8) noreturn {
 }
 
 pub fn getRoundDataFromFilePart1(input_file_path: []const u8, alloc: std.mem.Allocator) !std.ArrayList(RoundData) {
+    const cwd = std.fs.cwd();
+
+    const input_file = try cwd.openFile(input_file_path, .{ .mode = std.fs.File.OpenMode.read_only });
+    defer input_file.close();
+
+    const input_file_reader = input_file.reader();
+    var read_buffer: [1024]u8 = undefined;
+
     var rounds = std.ArrayList(RoundData).init(alloc);
-    _ = input_file_path;
+    errdefer rounds.deinit();
+
+    while (try input_file_reader.readUntilDelimiterOrEof(read_buffer[0..], '\n')) |line| {
+        if (line.len != 3) {
+            return ReadInputError.InvalidLineFormat;
+        }
+
+        if (line[1] != ' ') {
+            return ReadInputError.InvalidLineFormat;
+        }
+
+        var opponent_choice_char = line[0];
+        var player_choice_char = line[2];
+
+        var opponent_choice = switch (opponent_choice_char) {
+            'A' => RpsChoice.rock,
+            'B' => RpsChoice.paper,
+            'C' => RpsChoice.scissors,
+            else => {
+                std.debug.assert(false);
+                return ReadInputError.InvalidLineFormat;
+            },
+        };
+
+        var player_choice = switch (player_choice_char) {
+            'X' => RpsChoice.rock,
+            'Y' => RpsChoice.paper,
+            'Z' => RpsChoice.scissors,
+            else => {
+                std.debug.assert(false);
+                return ReadInputError.InvalidLineFormat;
+            },
+        };
+
+        try rounds.append(.{ .player_choice = player_choice, .opponent_choice = opponent_choice });
+    }
+
     return rounds;
 }
 
@@ -59,28 +104,28 @@ pub fn calculateRoundResult(round: RoundData) RpsResult {
     if (round.opponent_choice == round.player_choice) {
         return RpsResult.draw;
     } else {
-        switch (round.player_choice) {
-            RpsChoice.rock => return if (round.opponent_choice == RpsChoice.scissors) RpsResult.win else RpsResult.lose,
-            RpsChoice.paper => return if (round.opponent_choice == RpsChoice.rock) RpsResult.win else RpsResult.lose,
-            RpsChoice.scissors => return if (round.opponent_choice == RpsChoice.rock) RpsResult.win else RpsResult.lose,
-        }
+        return switch (round.player_choice) {
+            RpsChoice.rock => if (round.opponent_choice == RpsChoice.scissors) RpsResult.win else RpsResult.lose,
+            RpsChoice.paper => if (round.opponent_choice == RpsChoice.rock) RpsResult.win else RpsResult.lose,
+            RpsChoice.scissors => if (round.opponent_choice == RpsChoice.rock) RpsResult.win else RpsResult.lose,
+        };
     }
 }
 
 pub fn calculateChoiceScore(player_choice: RpsChoice) u32 {
-    switch (player_choice) {
-        RpsChoice.rock => return 1,
-        RpsChoice.paper => return 2,
-        RpsChoice.scissors => return 3,
-    }
+    return switch (player_choice) {
+        RpsChoice.rock => 1,
+        RpsChoice.paper => 2,
+        RpsChoice.scissors => 3,
+    };
 }
 
 pub fn calculateResultScore(round_result: RpsResult) u32 {
-    switch (round_result) {
-        RpsResult.lose => return 0,
-        RpsResult.draw => return 3,
-        RpsResult.win => return 6,
-    }
+    return switch (round_result) {
+        RpsResult.lose => 0,
+        RpsResult.draw => 3,
+        RpsResult.win => 6,
+    };
 }
 
 // FIXME: stale test from the stub file creation
