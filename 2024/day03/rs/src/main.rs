1952dd2bb050d8714245c921db42ae1d566211f8
Scott Munro <scottnmunro@gmail.com>
2024-12-02 21:51:42 -0800

    pt 1 done. had to fact check in python because my rust version was broken in a few ways

--- 2024/day03/rs/src/main.rs
+++ 2024/day03/rs/src/main.rs
@@ -1,21 +1,34 @@
 use input_helpers;
-use std::process::ExitCode;
+use std::{io::Read, process::ExitCode};
 
 type MemoryLine = String;
 
 type MulOp = (isize, isize);
 
-fn read_memory_lines(filename: &str) -> Result<Vec<MemoryLine>, String> {
-    let lines = input_helpers::read_lines(filename);
-    let mut memory_lines: Vec<MemoryLine> = Vec::new();
-    for line in lines {
-        memory_lines.push(line);
+fn read_memory_line(filename: &str) -> Result<MemoryLine, String> {
+    let mut memory_line = String::new();
+
+    // let lines = input_helpers::read_lines(filename);
+    // for line in lines {
+    //     memory_line.push_str(&line);
+    // }
+
+    let mut file = std::fs::File::open(filename).unwrap();
+    let res = file.read_to_string(&mut memory_line);
+    match res {
+        Ok(_) => Ok(memory_line),
+        Err(e) => Err(format!("Failed to read file! {}", e)),
     }
-    Ok(memory_lines)
 }
 
-fn extract_mul_ops(report_data: &[MemoryLine]) -> Vec<MulOp> {
-    unimplemented!();
+fn extract_mul_ops(memory_line: &str) -> Vec<MulOp> {
+    
+    let re = regex::Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)").unwrap();
+    let mut results: Vec<MulOp> = vec![];
+    for (_, [op1, op2]) in re.captures_iter(memory_line).map(|c| c.extract()) {
+        results.push((op1.parse().unwrap(), op2.parse().unwrap()))
+    }
+    results
 }
 
 fn main() -> ExitCode {
@@ -27,26 +40,21 @@ fn main() -> ExitCode {
 
     let filename: &str = &args[0];
 
-    let parse_result = read_memory_lines(filename);
-    let memory_lines = match parse_result {
-        Ok(parsed_memory_lines) => parsed_memory_lines,
+    let parse_result = read_memory_line(filename);
+    let memory_line = match parse_result {
+        Ok(parsed_memory_line) => parsed_memory_line,
         Err(e) => {
             println!("Invalid input! {}", e);
             return ExitCode::FAILURE;
         }
     };
 
-    let mul_ops = extract_mul_ops(&memory_lines);
+    let mul_ops = extract_mul_ops(&memory_line);
     let mul_sum = mul_ops.iter().map(|mul_op| mul_op.0 * mul_op.1).fold(0, |acc, v| acc + v);
-    println!("RESULT: {}", mul_sum);
-    for (i, mul_op) in mul_ops.iter().enumerate() {
-        if i == 0 {
-            print!("    = ");
-        } else {
-            print!(" + ");
-        }
-        print!("({} * {})", mul_op.0, mul_op.1)
+    for mul_op in mul_ops.iter() {
+        println!("+ ({} * {})", mul_op.0, mul_op.1)
     }
+    println!("= {}", mul_sum);
 
     return ExitCode::SUCCESS;
 }
