1d28d86521fc826a9c6c92351c11c70a6f9b7c95
Scott Munro <scottnmunro@gmail.com>
2024-12-07 00:56:55 -0800

    cleanup comments to delete

--- 2024/day07/rs/src/main.rs
+++ 2024/day07/rs/src/main.rs
@@ -198,16 +198,16 @@ fn main() -> ExitCode {
     let solved_equations_pt1: Vec<(Equation, Vec<Operation>)> = equations
         .iter()
         .map(|eq| (eq.clone(), solve_recursive_any_pt1(&eq)))
-        .filter(|(eq, solution)| solution.is_some())
+        .filter(|(_eq, solution)| solution.is_some())
         .map(|(eq, solution)| (eq, solution.unwrap()))
         .collect();
 
-    for (eq, _) in &solved_equations_pt1 {
-        println!("Found sol for {:?}", eq);
-    }
+    // for (eq, _) in &solved_equations_pt1 {
+    //     println!("Found sol for {:?}", eq);
+    // }
 
     let sum_solvable_results_pt1: usize =
-        solved_equations_pt1.iter().map(|(eq, sol)| eq.result).sum();
+        solved_equations_pt1.iter().map(|(eq, _sol)| eq.result).sum();
     println!("Sum of solution results: {}", sum_solvable_results_pt1);
 
     println!("");
@@ -217,84 +217,17 @@ fn main() -> ExitCode {
     let solved_equations_pt2: Vec<(Equation, Vec<Operation>)> = equations
         .iter()
         .map(|eq| (eq.clone(), solve_recursive_any_pt2(&eq)))
-        .filter(|(eq, solution)| solution.is_some())
+        .filter(|(_eq, solution)| solution.is_some())
         .map(|(eq, solution)| (eq, solution.unwrap()))
         .collect();
 
-    for (eq, _) in &solved_equations_pt2 {
-        println!("Found sol for {:?}", eq);
-    }
+    // for (eq, _) in &solved_equations_pt2 {
+    //     println!("Found sol for {:?}", eq);
+    // }
 
     let sum_solvable_results_pt2: usize =
-        solved_equations_pt2.iter().map(|(eq, sol)| eq.result).sum();
+        solved_equations_pt2.iter().map(|(eq, _sol)| eq.result).sum();
     println!("Sum of solution results: {}", sum_solvable_results_pt2);
-
-    /*
-    let player_initial_state = player_state.clone();
-
-    let pt1_start_time = std::time::Instant::now();
-
-    let mut player_space_history = std::collections::HashSet::new();
-
-    //print_board_state(&grid, &player_state);
-
-    loop {
-        player_space_history.insert(player_state.pos);
-
-        player_state = simulate_board_step(&grid, &player_state);
-        //print_board_state(&grid, &player_state);
-
-        if grid.is_pos_out_of_bounds(player_state.pos.row, player_state.pos.col) {
-            break;
-        }
-    }
-
-    let pt1_time = pt1_start_time.elapsed();
-
-    println!(
-        "{} unique player positions",
-        player_space_history.len()
-    );
-
-    //print_move_history(&grid, &player_space_history);
-
-    println!("TIME: ({:0.06}s)", pt1_time.as_secs_f64());
-
-    println!("");
-
-    let pt2_start_time = std::time::Instant::now();
-
-    let mut looping_obstructions: Vec<GridPos> = Vec::new();
-
-    let obstruction_candidates = {
-        let mut obstruction_candidates = player_space_history.clone();
-        obstruction_candidates.remove(&player_initial_state.pos);
-        obstruction_candidates
-    };
-
-    for visited_player_space in obstruction_candidates {
-        let mut obstructed_grid = Grid { width: grid.width, height: grid.height, cells: grid.cells.clone() };
-        {
-            let cell_ref = obstructed_grid.get_cell_mut(visited_player_space.row, visited_player_space.col);
-            assert!(*cell_ref == Cell::Empty);
-            *cell_ref = Cell::Obstacle;
-        }
-
-        let obstruction_loops_player = test_for_simulation_loop(&obstructed_grid, &player_initial_state);
-        if obstruction_loops_player {
-            looping_obstructions.push(visited_player_space);
-        }
-
-        // println!("Placing obstruction at (r:{},c:{}) looped player? {}",
-        //     visited_player_space.row,
-        //     visited_player_space.col,
-        //     obstruction_loops_player);
-    }
-
-    let pt2_time = pt2_start_time.elapsed();
-    println!("Found {} obstructions which cause loops", looping_obstructions.len());
-    println!("TIME: ({:0.06}s)", pt2_time.as_secs_f64());
-    */
 
     return ExitCode::SUCCESS;
 }
