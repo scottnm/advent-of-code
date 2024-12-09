b303c88798bb79b567d88e84c6e3235b6c121fb6
Scott Munro <scottnmunro@gmail.com>
2024-12-09 00:06:00 -0800

    pt1 done

--- 2024/day09/rs/src/main.rs
+++ 2024/day09/rs/src/main.rs
@@ -1,11 +1,22 @@
 use input_helpers;
-use std::{fmt::format, process::ExitCode};
+use std::{fmt::format, ops::Index, process::ExitCode};
 use itertools::Itertools;
 
-#[derive(Debug)]
+#[derive(Debug, Clone, Copy)]
+struct FileChunk {
+    id: u32,
+    block_count: u8,
+}
+
+#[derive(Debug, Clone, Copy)]
+struct FreeSpaceChunk {
+    block_count: u8,
+}
+
+#[derive(Debug, Clone, Copy)]
 enum DiskChunk {
-    File{id: u32, block_count: u8}, // block_count is u8 since all each file's length is limited to a single digit
-    FreeSpace{block_count: u8},
+    File(FileChunk), // block_count is u8 since all each file's length is limited to a single digit
+    FreeSpace(FreeSpaceChunk),
 }
 
 fn read_disk_layout(filename: &str) -> Result<Vec<DiskChunk>, String> {
@@ -29,11 +40,11 @@ fn read_disk_layout(filename: &str) -> Result<Vec<DiskChunk>, String> {
         };
 
         let disk_chunk = if disk_chunk_toggle {
-            let disk_chunk = DiskChunk::File { id: next_file_id, block_count: disk_chunk_len };
+            let disk_chunk = DiskChunk::File(FileChunk{id: next_file_id, block_count: disk_chunk_len});
             next_file_id += 1;
             disk_chunk
         } else {
-            DiskChunk::FreeSpace { block_count: disk_chunk_len }
+            DiskChunk::FreeSpace(FreeSpaceChunk { block_count: disk_chunk_len })
         };
 
         disk_chunk_toggle = !disk_chunk_toggle;
@@ -42,6 +53,94 @@ fn read_disk_layout(filename: &str) -> Result<Vec<DiskChunk>, String> {
     }
 
     Ok(disk_chunks)
+}
+
+fn compact_disk(disk_chunks: &[DiskChunk]) -> Vec<DiskChunk> {
+    let mut compacted_disk_chunks = disk_chunks.to_vec();
+
+    fn is_free_space(chunk: &DiskChunk) -> bool {
+        match chunk {
+            DiskChunk::File(_) => false,
+            DiskChunk::FreeSpace(_) => true,
+        }
+    }
+
+    fn find_free_space(disk_chunks: &[DiskChunk], offset: usize) -> Option<usize> {
+        disk_chunks.iter().skip(offset).position(is_free_space).map(|idx_with_skip| idx_with_skip + offset)
+    }
+
+    let mut next_free_chunk_search_offset = 0;
+    loop {
+        while !compacted_disk_chunks.is_empty() && is_free_space(compacted_disk_chunks.last().unwrap()) {
+            compacted_disk_chunks.pop();
+        }
+
+        let next_free_chunk_idx_search = find_free_space(&compacted_disk_chunks, next_free_chunk_search_offset);
+
+        let next_free_chunk_idx = if let Some(next_free_chunk_idx) = next_free_chunk_idx_search {
+            next_free_chunk_idx
+        } else {
+            break;
+        };
+
+        // There has to be at least one free chunk from the beginning of the search offset and one non-free chunk at the end.
+        assert!((compacted_disk_chunks.len() - next_free_chunk_search_offset) >= 2);
+
+        let free_chunk = match &compacted_disk_chunks[next_free_chunk_idx] {
+            DiskChunk::FreeSpace(free_chunk) => free_chunk.clone(),
+            // FIXME: there's got to be a more elegant way to do this. Maybe it means that
+            // what I'm doing getting indexes isn't the right way to do this or using enums isn't the best tool here
+            _ => panic!("Unexpected chunk type! Expected 'free'"),
+        };
+
+        let tail_file_chunk = match compacted_disk_chunks.last().unwrap() {
+            DiskChunk::File(file_chunk) => file_chunk.clone(),
+            // FIXME: there's got to be a more elegant way to do this. Maybe it means that
+            // what I'm doing getting indexes isn't the right way to do this or using enums isn't the best tool here
+            _ => panic!("Unexpected chunk type! Expected 'file'"),
+        };
+
+        if free_chunk.block_count < tail_file_chunk.block_count {
+            compacted_disk_chunks[next_free_chunk_idx] = DiskChunk::File(FileChunk{id: tail_file_chunk.id, block_count: free_chunk.block_count});
+            *compacted_disk_chunks.last_mut().unwrap() = DiskChunk::File(FileChunk{id: tail_file_chunk.id, block_count: tail_file_chunk.block_count - free_chunk.block_count});
+        } else if free_chunk.block_count == tail_file_chunk.block_count {
+            compacted_disk_chunks[next_free_chunk_idx] = DiskChunk::File(FileChunk{id: tail_file_chunk.id, block_count: free_chunk.block_count});
+            compacted_disk_chunks.pop();
+        } else {
+            let remaining_free_space = free_chunk.block_count - tail_file_chunk.block_count;
+            compacted_disk_chunks[next_free_chunk_idx] = DiskChunk::File(tail_file_chunk);
+            compacted_disk_chunks.pop();
+            compacted_disk_chunks.insert(next_free_chunk_idx + 1, DiskChunk::FreeSpace(FreeSpaceChunk{block_count: remaining_free_space}));
+        }
+
+        next_free_chunk_search_offset = next_free_chunk_idx;
+    }
+
+    compacted_disk_chunks
+}
+
+fn calculate_checksum(disk_chunks: &[DiskChunk]) -> usize {
+    // FIXME: I should be able to short-circuit this if I know it's compacted.
+    // anyway to make the caller have to guarantee that? or to safely and quickly check it myself?
+    let mut checksum: usize = 0;
+
+    let mut block_idx: usize = 0;
+    for chunk in disk_chunks {
+        match chunk {
+            DiskChunk::File(FileChunk { id, block_count }) => {
+                let block_count = *block_count as usize;
+                let block_idx_sum: usize = (block_idx..(block_idx+block_count)).sum();
+                let id = *id as usize;
+                checksum += block_idx_sum * id;
+                block_idx += block_count;
+            },
+            DiskChunk::FreeSpace(FreeSpaceChunk { block_count }) => {
+                block_idx += (*block_count as usize);
+            },
+        }
+    }
+    
+    checksum
 }
 
 fn main() -> ExitCode {
@@ -62,7 +161,15 @@ fn main() -> ExitCode {
         }
     };
 
-    dbg!(disk_chunks);
+    {
+        let compacted_disk_chunks = compact_disk(&disk_chunks);
+        let checksum = calculate_checksum(&compacted_disk_chunks);
+        println!("Pt 1: checksum = {}", checksum);
+        if compacted_disk_chunks.len() < 10 {
+            dbg!(compacted_disk_chunks);
+        }
+    }
+
     /*
     dump_tower_grid(&tower_grid);
 
@@ -90,4 +197,23 @@ fn main() -> ExitCode {
     */
 
     return ExitCode::SUCCESS;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        assert_eq!(4, 4);
+    }
+
+    #[test]
+    fn test_skip_position() {
+        let nums = [0, 1, 2, 3, 4, 5, 6];
+        assert_eq!(nums.iter().position(|n| *n == 2), Some(2));
+        //fails: assert_eq!(nums.iter().skip(2).position(|n| *n == 2), Some(2));
+        assert_eq!(nums.iter().skip(3).position(|n| *n == 2), None);
+        //fails: assert_eq!(nums.iter().skip(3).position(|n| *n == 6), Some(6));
+    }
 }
