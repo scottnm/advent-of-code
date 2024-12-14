ba88056683147a83df09ca0c25666e5ba98dea1a
Scott Munro <scottnmunro@gmail.com>
2024-12-13 23:11:54 -0800

    Day14 pt1 done. Will need to come back for pt 2 Squashed commit of the following:
    commit d7d182802e9e612138b88b161b026b7c69117b72
    Author: Scott Munro <scottnmunro@gmail.com>
    Date:   Fri Dec 13 23:11:46 2024 -0800
    
        cleanup
    
    commit 9188a653d4c634fb696fe96ed56834f1e72d94e4
    Author: Scott Munro <scottnmunro@gmail.com>
    Date:   Fri Dec 13 23:09:03 2024 -0800
    
        format
    
    commit e07740aa04180a1aa8c6868451fc71ebed68ba9c
    Author: Scott Munro <scottnmunro@gmail.com>
    Date:   Fri Dec 13 23:08:36 2024 -0800
    
        pt2 bad idea. just dump tree states and see if I can find something within the first few hundred prints. i did not fidn it lol
    
    commit 7b97df9e180a13a7429050ed345bf7f66c70cc78
    Author: Scott Munro <scottnmunro@gmail.com>
    Date:   Fri Dec 13 22:23:19 2024 -0800
    
        pt1 done! my first dumb typo wrapping the height. mixed up width and height and x and y on one of the wrapping calculations
    
    commit f7a6de925fc47fe046bd99b77c8b981894c89147
    Author: Scott Munro <scottnmunro@gmail.com>
    Date:   Fri Dec 13 22:06:46 2024 -0800
    
        first attempt for part 1 not working on sample input
    
    commit 191babd9569d70cde1f367bcecf3056e4f99d5fa
    Author: Scott Munro <scottnmunro@gmail.com>
    Date:   Fri Dec 13 21:51:10 2024 -0800
    
        skeleton of solution
    
    commit ce059a20b11783f411bf7ae2d85a695a2a5198f1
    Author: Scott Munro <scottnmunro@gmail.com>
    Date:   Fri Dec 13 21:41:06 2024 -0800
    
        parsing input
    
    commit 530d5e5dfd4c5d6e055864200988204cefa66c49
    Author: Scott Munro <scottnmunro@gmail.com>
    Date:   Fri Dec 13 21:01:55 2024 -0800
    
        starting point for input
    
    commit 5036273def7fa407e2411e021ccca432aa96fb84
    Author: Scott Munro <scottnmunro@gmail.com>
    Date:   Fri Dec 13 20:59:47 2024 -0800
    
        starting point
    
    commit 8257fc6d64894bd29f390ad2ccad7d99c6d74543
    Author: Scott Munro <scottnmunro@gmail.com>
    Date:   Fri Dec 13 20:54:31 2024 -0800
    
        day14 starting template

---
+++ 2024/day14/rs/src/main.rs
@@ -0,0 +1,296 @@
+use input_helpers;
+use std::process::ExitCode;
+
+#[derive(Clone, Copy, PartialEq, Eq, Debug)]
+struct Vec2 {
+    x: isize,
+    y: isize,
+}
+
+impl std::fmt::Display for Vec2 {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "(x:{},y:{})", self.x, self.y)
+    }
+}
+
+#[derive(Clone, Copy, PartialEq, Eq, Debug)]
+struct Robot {
+    pos: Vec2,
+    vel: Vec2,
+}
+
+struct RobotArea {
+    width: usize,
+    height: usize,
+}
+
+fn dump_grid_to_str(title: &str, robot_area: &RobotArea, robots: &[Robot]) -> String {
+    let mut string_buf = String::with_capacity(robot_area.width);
+
+    string_buf.push_str(&format!("{}:\n", title));
+    for r in 0..robot_area.height as isize {
+        for c in 0..robot_area.width as isize {
+            let pos = Vec2 { x: c, y: r };
+            let robots_in_pos = robots.iter().filter(|r| r.pos == pos).count();
+            let cell_char = match robots_in_pos {
+                0 => '.',
+                1..=9 => (('0' as usize) + robots_in_pos) as u8 as char,
+                10..=35 => (('A' as usize) + (robots_in_pos - 10)) as u8 as char,
+                36..=61 => (('a' as usize) + (robots_in_pos - 36)) as u8 as char,
+                _ => panic!("not enough chars to represent {}", robots_in_pos),
+            };
+            string_buf.push(cell_char);
+            string_buf.push(' ');
+        }
+        string_buf.push('\n');
+    }
+    string_buf
+}
+
+fn dump_grid(title: &str, robot_area: &RobotArea, robots: &[Robot]) {
+    print!("{}", dump_grid_to_str(title, robot_area, robots));
+}
+
+fn read_robots(filename: &str) -> Result<(RobotArea, Vec<Robot>), String> {
+    let lines: Vec<String> = input_helpers::read_lines(filename).collect();
+
+    if lines.is_empty() {
+        return Err(format!(
+            "Need at least one line for width/height of robot area"
+        ));
+    }
+
+    let area_line = &lines[0];
+    let mut area_line_split = area_line.split_ascii_whitespace();
+
+    let width: usize = area_line_split
+        .next()
+        .unwrap()
+        .parse()
+        .map_err(|_| String::from("Failed to parse width"))?;
+
+    let height: usize = area_line_split
+        .next()
+        .ok_or(String::from("Missing height value on first list"))?
+        .parse()
+        .map_err(|_| String::from("Failed to parse width"))?;
+
+    let robot_area = RobotArea { width, height };
+
+    let robot_line_re = regex::Regex::new(r"p=(-?\d+),(-?\d+)\s+v=(-?\d+),(-?\d+)").unwrap();
+
+    let mut robots = vec![];
+
+    for line in &lines[1..] {
+        let captures = robot_line_re.captures(line);
+        let robot_match = captures.ok_or(format!(
+            "line '{}' did not match expected robot format",
+            line
+        ))?;
+
+        let px: isize = robot_match
+            .get(1)
+            .unwrap()
+            .as_str()
+            .parse()
+            .map_err(|_| format!("X position could not be parsed as int! '{}'", line))?;
+
+        let py: isize = robot_match
+            .get(2)
+            .unwrap()
+            .as_str()
+            .parse()
+            .map_err(|_| format!("Y position could not be parsed as int! '{}'", line))?;
+
+        let vx: isize = robot_match
+            .get(3)
+            .unwrap()
+            .as_str()
+            .parse()
+            .map_err(|_| format!("X velocity could not be parsed as int! '{}'", line))?;
+
+        let vy: isize = robot_match
+            .get(4)
+            .unwrap()
+            .as_str()
+            .parse()
+            .map_err(|_| format!("Y velocity could not be parsed as int! '{}'", line))?;
+
+        let robot = Robot {
+            pos: Vec2 { x: px, y: py },
+            vel: Vec2 { x: vx, y: vy },
+        };
+        robots.push(robot);
+    }
+
+    Ok((robot_area, robots))
+}
+
+fn step_by_step_simulation(
+    robots: &mut [Robot],
+    robot_area: &RobotArea,
+    simulation_step_count: usize,
+    print_grid: bool,
+    in_place_print: bool,
+) {
+    let cursor_move = format!("\x1b[{}A", robot_area.height + 1);
+
+    // FIXME:
+    // This is horribly naive. There are much faster ways to do this. Namely, I don't actually have to loop.
+    // I can just multiple all of the moves together and do one calculated adjustment back onto the grid that
+    // does all wrapping at once. But I'm keeping it naive for now since I don't know what part 2 will be like.
+    for i in 0..simulation_step_count {
+        for robot in robots.iter_mut() {
+            robot.pos.x += robot.vel.x;
+            robot.pos.y += robot.vel.y;
+
+            if robot.pos.x < 0 {
+                robot.pos.x += (robot_area.width) as isize;
+            } else if robot.pos.x >= robot_area.width as isize {
+                robot.pos.x -= (robot_area.width) as isize;
+            }
+
+            if robot.pos.y < 0 {
+                robot.pos.y += (robot_area.height) as isize;
+            } else if robot.pos.y >= robot_area.height as isize {
+                robot.pos.y -= (robot_area.height) as isize;
+            }
+        }
+
+        if print_grid {
+            let grid_str =
+                dump_grid_to_str(&format!("after step {:03}", i + 1), robot_area, &robots);
+            print!("{}", grid_str);
+        }
+
+        if in_place_print && (i < simulation_step_count - 1) {
+            std::thread::sleep(std::time::Duration::from_millis(250));
+            print!("{}", cursor_move);
+        }
+    }
+}
+
+fn count_robots_in_quadrants(
+    robots: &[Robot],
+    robot_area: &RobotArea,
+) -> (usize, usize, usize, usize) {
+    /*
+    |----|-----
+    | Q1 | Q2 |
+    |----|-----
+    | Q3 | Q4 |
+    |----|-----
+    */
+
+    struct QuadrantBounds {
+        topleft: Vec2,
+        bottomright: Vec2,
+    }
+
+    fn in_quadrant_bounds(bounds: &QuadrantBounds, pos: &Vec2) -> bool {
+        pos.x >= bounds.topleft.x
+            && pos.x <= bounds.bottomright.x
+            && pos.y >= bounds.topleft.y
+            && pos.y <= bounds.bottomright.y
+    }
+
+    // FIXME:  assume width and height are odd values for now. Its the sizes provided in both the sample problem
+    // and the real pt1 input. Assuming odd makes it easier to calculate area bounds or now
+    assert!(robot_area.height % 2 == 1);
+    assert!(robot_area.width % 2 == 1);
+    let mid_x = (robot_area.width / 2) as isize;
+    let mid_y = (robot_area.height / 2) as isize;
+    let top_x = (robot_area.width - 1) as isize;
+    let top_y = (robot_area.height - 1) as isize;
+
+    let q1_bounds = QuadrantBounds {
+        topleft: Vec2 { x: 0, y: 0 },
+        bottomright: Vec2 {
+            x: mid_x - 1,
+            y: mid_y - 1,
+        },
+    };
+    let q2_bounds = QuadrantBounds {
+        topleft: Vec2 { x: mid_x + 1, y: 0 },
+        bottomright: Vec2 {
+            x: top_x,
+            y: mid_y - 1,
+        },
+    };
+    let q3_bounds = QuadrantBounds {
+        topleft: Vec2 { x: 0, y: mid_y + 1 },
+        bottomright: Vec2 {
+            x: mid_x - 1,
+            y: top_y,
+        },
+    };
+    let q4_bounds = QuadrantBounds {
+        topleft: Vec2 {
+            x: mid_x + 1,
+            y: mid_y + 1,
+        },
+        bottomright: Vec2 { x: top_x, y: top_y },
+    };
+
+    let mut quadrant_counts = (0, 0, 0, 0);
+
+    for robot in robots {
+        if in_quadrant_bounds(&q1_bounds, &robot.pos) {
+            quadrant_counts.0 += 1;
+        } else if in_quadrant_bounds(&q2_bounds, &robot.pos) {
+            quadrant_counts.1 += 1;
+        } else if in_quadrant_bounds(&q3_bounds, &robot.pos) {
+            quadrant_counts.2 += 1;
+        } else if in_quadrant_bounds(&q4_bounds, &robot.pos) {
+            quadrant_counts.3 += 1;
+        }
+    }
+
+    quadrant_counts
+}
+
+fn main() -> ExitCode {
+    let args: Vec<String> = std::env::args().skip(1).collect();
+    if args.is_empty() {
+        println!("Not enough args");
+        return ExitCode::FAILURE;
+    }
+
+    let filename: &str = &args[0];
+
+    let parse_result = read_robots(filename);
+    let (robot_area, robots) = match parse_result {
+        Ok(result) => result,
+        Err(e) => {
+            println!("Invalid input! {}", e);
+            return ExitCode::FAILURE;
+        }
+    };
+
+    {
+        if robot_area.width * robot_area.height < 250 {
+            dump_grid("pt 1. start state", &robot_area, &robots);
+        }
+
+        let mut simulated_robots = robots.clone();
+        step_by_step_simulation(&mut simulated_robots, &robot_area, 10000, true, true);
+        let quadrant_counts = count_robots_in_quadrants(&simulated_robots, &robot_area);
+
+        if robot_area.width * robot_area.height < 250 {
+            dump_grid("pt 1. end state", &robot_area, &simulated_robots);
+        }
+
+        let total_safety_factor =
+            quadrant_counts.0 * quadrant_counts.1 * quadrant_counts.2 * quadrant_counts.3;
+        println!(
+            "Pt 1. Total safety factor: {} = {} * {} * {} * {}",
+            total_safety_factor,
+            quadrant_counts.0,
+            quadrant_counts.1,
+            quadrant_counts.2,
+            quadrant_counts.3
+        );
+    }
+
+    return ExitCode::SUCCESS;
+}
