6571816be2af83b5323cef9ddb8ac074af9420b2
Scott Munro <scottnmunro@gmail.com>
2024-12-15 22:49:31 -0800

    beginning input parsing done

--- 2024/day16/rs/src/main.rs
+++ 2024/day16/rs/src/main.rs
@@ -5,71 +5,47 @@ use std::process::ExitCode;
 #[derive(Clone, Copy, Eq, PartialEq, Hash, Debug)]
 enum Space {
     Empty,
-    Box,
     Wall,
 }
 
 #[derive(Clone, Copy, Eq, PartialEq, Hash, Debug)]
-enum Move {
-    Left,
-    Right,
-    Up,
-    Down,
+enum Direction {
+    North,
+    South,
+    East,
+    West,
 }
 
-type Warehouse = Grid<Space>;
+#[derive(Clone, Copy, Eq, PartialEq, Hash, Debug)]
+enum Move {
+    Forward,
+    TurnCW,
+    TurnCCW,
+}
 
-fn read_input(filename: &str) -> Result<(Grid<Space>, GridPos, Vec<Move>), String> {
+struct StartingState {
+    maze: Grid<Space>,
+    start_pos: GridPos,
+    end_pos: GridPos,
+    starting_dir: Direction,
+}
+
+type MazePath = Vec<Move>;
+
+fn read_input(filename: &str) -> Result<StartingState, String> {
     let lines: Vec<String> = input_helpers::read_lines(filename).collect();
 
-    if lines.len() < 4 {
-        return Err(format!(
-            "Invalid input! Require at least 4 lines. Had {}",
-            lines.len()
-        ));
+    if lines.is_empty() {
+        return Err(format!("Invalid input! no lines/maze"));
     }
 
-    fn is_full_wall_line(line: &str) -> bool {
-        !line.is_empty() && line.chars().all(|c| c == '#')
-    }
+    let width = lines[0].len();
+    let height = lines.len();
 
-    let first_grid_line = &lines[0];
-    if !is_full_wall_line(first_grid_line) {
-        return Err(format!(
-            "Invalid input! First line must be all '#'! Found '{}'",
-            first_grid_line
-        ));
-    }
-
-    let last_grid_line_idx = lines[1..]
-        .iter()
-        .enumerate()
-        .rev()
-        .find(|(_idx, line)| is_full_wall_line(line))
-        .map(|(idx, _line)| idx + 1) // +1 since we start iterating from 1
-        .ok_or(String::from("Missing trailing wall line"))?;
-
-    let grid_lines = &lines[0..last_grid_line_idx + 1];
-
-    let separator_line_idx = last_grid_line_idx + 1;
-    if separator_line_idx >= lines.len() || lines[separator_line_idx] != "" {
-        return Err(format!(
-            "Expected empty line on line {}! Found '{}'",
-            separator_line_idx, lines[separator_line_idx]
-        ));
-    }
-
-    let move_lines = &lines[separator_line_idx + 1..];
-    if move_lines.is_empty() {
-        return Err(String::from("Missing move line(s)"));
-    }
-
-    let width = first_grid_line.len();
-    let height = grid_lines.len();
-
-    let mut robot_pos = None;
-    let mut warehouse_cells: Vec<Space> = vec![];
-    for (line_idx, line) in grid_lines.iter().enumerate() {
+    let mut start_pos = None;
+    let mut end_pos = None;
+    let mut maze_cells: Vec<Space> = vec![];
+    for (line_idx, line) in lines.iter().enumerate() {
         if line.len() != width {
             return Err(format!(
                 "Grid must have consistent line widths! Expected {} found {}",
@@ -79,62 +55,59 @@ fn read_input(filename: &str) -> Result<(Grid<Space>, GridPos, Vec<Move>), Strin
         }
 
         for (chr_idx, c) in line.chars().enumerate() {
-            if c == '@' {
-                let new_robot_pos = GridPos {
-                    row: line_idx as isize,
-                    col: chr_idx as isize,
-                };
-                if let Some(robot_pos) = robot_pos {
-                    return Err(format!(
-                        "Repeat robot_pos found {}! First found at {}",
-                        new_robot_pos, robot_pos
-                    ));
+            match c {
+                'S' => {
+                    let new_starting_pos = GridPos {
+                        row: line_idx as isize,
+                        col: chr_idx as isize,
+                    };
+                    if let Some(pos) = start_pos {
+                        return Err(format!(
+                            "Repeat starting position found {}! First found at {}",
+                            new_starting_pos, pos
+                        ));
+                    }
+                    maze_cells.push(Space::Empty);
+                    start_pos = Some(new_starting_pos);
                 }
-
-                warehouse_cells.push(Space::Empty);
-                robot_pos = Some(new_robot_pos);
-            } else {
-                let cell = match c {
-                    '.' => Space::Empty,
-                    'O' => Space::Box,
-                    '#' => Space::Wall,
-                    _ => return Err(format!("Invalid warehouse char! {}", c)),
-                };
-                warehouse_cells.push(cell);
+                'E' => {
+                    let new_end_pos = GridPos {
+                        row: line_idx as isize,
+                        col: chr_idx as isize,
+                    };
+                    if let Some(pos) = end_pos {
+                        return Err(format!(
+                            "Repeat end position found {}! First found at {}",
+                            new_end_pos, pos
+                        ));
+                    }
+                    maze_cells.push(Space::Empty);
+                    end_pos = Some(new_end_pos);
+                }
+                '.' => maze_cells.push(Space::Empty),
+                '#' => maze_cells.push(Space::Wall),
+                _ => return Err(format!("Invalid maze char! {}", c)),
             }
         }
     }
 
-    let mut moves = vec![];
-    for line in move_lines {
-        for c in line.chars() {
-            let move_instr = match c {
-                '<' => Move::Left,
-                '^' => Move::Up,
-                '>' => Move::Right,
-                'v' => Move::Down,
-                _ => return Err(format!("Invalid move char! {}", c)),
-            };
-
-            moves.push(move_instr);
-        }
-    }
-
-    let warehouse_grid = Grid::<Space> {
+    let maze_grid = Grid::<Space> {
         width: width,
         height: height,
-        cells: warehouse_cells,
+        cells: maze_cells,
     };
 
-    let result = (
-        warehouse_grid,
-        robot_pos.ok_or(String::from("Missing robot pos in input"))?,
-        moves,
-    );
+    let result = StartingState {
+        maze: maze_grid,
+        start_pos: start_pos.ok_or(String::from("No start pos in grid"))?,
+        end_pos: end_pos.ok_or(String::from("No end pos in grid"))?,
+        starting_dir: Direction::East,
+    };
 
     Ok(result)
 }
 
+/* FIXME: remove if unneeded
 fn dump_warehouse(warehouse: &Warehouse, robot_pos: &GridPos) -> String {
     let mut buf = String::with_capacity((warehouse.width + 1) * warehouse.height);
     for r in 0..(warehouse.height as isize) {
@@ -162,69 +135,20 @@ fn print_warehouse(title: Option<&str>, warehouse: &Warehouse, robot_pos: &GridP
     }
     println!("{}", dump_warehouse(warehouse, robot_pos));
 }
+*/
 
-fn do_move(warehouse: &mut Warehouse, robot_pos: &mut GridPos, move_instr: Move) {
-    let (row_offset, col_offset) = match move_instr {
-        Move::Up => (-1, 0),
-        Move::Left => (0, -1),
-        Move::Down => (1, 0),
-        Move::Right => (0, 1),
-    };
-
-    let next_cell_pos = GridPos {
-        row: robot_pos.row + row_offset,
-        col: robot_pos.col + col_offset,
-    };
-    if warehouse.is_pos_out_of_bounds(next_cell_pos.row, next_cell_pos.col) {
-        return;
-    }
-
-    fn recursive_move_boxes(
-        row_offset: isize,
-        col_offset: isize,
-        warehouse: &mut Warehouse,
-        box_pos: &GridPos,
-    ) -> bool {
-        let next_cell_pos = GridPos {
-            row: box_pos.row + row_offset,
-            col: box_pos.col + col_offset,
-        };
-        if warehouse.is_pos_out_of_bounds(next_cell_pos.row, next_cell_pos.col) {
-            return false;
-        }
-
-        match warehouse.get_cell(next_cell_pos.row, next_cell_pos.col) {
-            Space::Wall => false,
-            Space::Box => {
-                if recursive_move_boxes(row_offset, col_offset, warehouse, &next_cell_pos) {
-                    *warehouse.get_cell_mut(next_cell_pos.row, next_cell_pos.col) = Space::Box;
-                    *warehouse.get_cell_mut(box_pos.row, box_pos.col) = Space::Empty;
-                    true
-                } else {
-                    false
-                }
-            }
-            Space::Empty => {
-                *warehouse.get_cell_mut(next_cell_pos.row, next_cell_pos.col) = Space::Box;
-                *warehouse.get_cell_mut(box_pos.row, box_pos.col) = Space::Empty;
-                true
-            }
-        }
-    }
-
-    match warehouse.get_cell(next_cell_pos.row, next_cell_pos.col) {
-        Space::Wall => (),                          // no move for wall
-        Space::Empty => *robot_pos = next_cell_pos, // move into the empty space
-        Space::Box => {
-            if recursive_move_boxes(row_offset, col_offset, warehouse, &next_cell_pos) {
-                *robot_pos = next_cell_pos;
-            }
-        }
-    }
+fn find_all_maze_paths(maze: &Grid<Space>, start_pos: GridPos, start_dir: Direction, end_pos: GridPos) -> Vec<MazePath> {
+    unimplemented!();
 }
 
-fn calc_box_gps(box_pos: &GridPos) -> usize {
-    ((100 * box_pos.row) + box_pos.col) as usize
+fn calculate_maze_move_score(maze_move: Move) -> usize {
+    match maze_move {
+        Move::Forward => 1,
+        Move::TurnCCW | Move::TurnCW => 1000,
+    }
+}
+fn calculate_maze_path_score(maze_path_moves: &[Move]) -> usize {
+    maze_path_moves.iter().cloned().map(calculate_maze_move_score).sum()
 }
 
 fn run(args: &[String]) -> Result<(), String> {
@@ -234,40 +158,31 @@ fn run(args: &[String]) -> Result<(), String> {
         .find(|a| a.as_str() == "-v" || a.as_str() == "--verbose")
         .is_some();
 
-    let (mut warehouse, mut robot_pos, moves) = read_input(filename)?;
+    let StartingState {
+        maze: maze,
+        start_pos: start_pos,
+        end_pos: end_pos,
+        starting_dir: starting_dir,
+    } = read_input(filename)?;
+
+    dbg!(&maze.width);
+    dbg!(&maze.height);
+    dbg!(&maze.cells);
+    dbg!(&start_pos);
+    dbg!(&end_pos);
+    dbg!(&starting_dir);
 
     {
-        if verbose {
-            print_warehouse(Some("warehouse start"), &warehouse, &robot_pos);
+        let maze_paths = find_all_maze_paths(&maze, start_pos, starting_dir, end_pos);
+        let min_maze_path_score = maze_paths
+            .iter()
+            .map(|maze_path| calculate_maze_path_score(maze_path))
+            .min();
+        if let Some(min_maze_path_score) = min_maze_path_score {
+            println!("pt 1: min score {}", min_maze_path_score);
+        } else {
+            println!("pt 1: no solutions to maze");
         }
-
-        for (i, move_instr) in moves.iter().enumerate() {
-            do_move(&mut warehouse, &mut robot_pos, *move_instr);
-            if verbose {
-                if i < moves.len() {
-                    print_warehouse(
-                        Some(&format!("after move {:03}", i)),
-                        &warehouse,
-                        &robot_pos,
-                    );
-                } else {
-                    print_warehouse(Some("warehouse end"), &warehouse, &robot_pos);
-                }
-            }
-        }
-
-        let mut sum_gps_coords = 0;
-        for r in 0..warehouse.height as isize {
-            for c in 0..warehouse.width as isize {
-                if let Space::Box = warehouse.get_cell(r, c) {
-                    let box_pos = GridPos { row: r, col: c };
-                    let box_gps = calc_box_gps(&box_pos);
-                    sum_gps_coords += box_gps;
-                }
-            }
-        }
-
-        println!("Pt 1: sum gps = {}", sum_gps_coords);
     }
 
     Ok(())
